042f24e7722e758f8e33c98c52b0f0a7f577ac82
liuyunge <7488516+liuyunge@user.noreply.gitee.com>
2021-04-20 13:28:38 +0800

    新增微服务

---
+++ distributed/service/order-center/order-service/src/test/java/com/yz/order/service/OrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yz.order.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
